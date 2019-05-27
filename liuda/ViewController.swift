6cd9007bf0267fba0ccc4fd34b5dd4b2635b9cfa
xuqiwei <1554091504@qq.com>
2019-05-27 20:54:25 +0800

    遛达代码上传

---
+++ liuda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  liuda
+//
+//  Created by 徐其威 on 2019/5/27.
+//  Copyright © 2019 徐其威. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ liudaTests/liudaTests.swift
@@ -0,0 +1,34 @@
+//
+//  liudaTests.swift
+//  liudaTests
+//
+//  Created by 徐其威 on 2019/5/27.
+//  Copyright © 2019 徐其威. All rights reserved.
+//
+
+import XCTest
+@testable import liuda
+
+class liudaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
